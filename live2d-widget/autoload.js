6e52f407289b16867176e425351c2542de3bef89
geother <geother@foxmail.com>
2022-04-05 15:34:54 +0800

    Site updated: 2022-04-05 15:34:54

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,63 +1,25 @@
-// 注意：live2d_path 参数应使用绝对路径
-// const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
+// 0.1.5
 const live2d_path = "/live2d-widget/";
 
-// 封装异步加载资源的方法
-function loadExternalResource(url, type) {
-	return new Promise((resolve, reject) => {
-		let tag;
+try {
+    $("<link>").attr({href: live2d_path+"waifu.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
+    $("<link>").attr({href: "https://cdn.jsdelivr.net/npm/font-awesome/css/font-awesome.min.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
+    $('body').append('<div id="waifu-toggle" class=""><span>看板娘</span></div><div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fa fa-lg fa-home"></span> <span class="fa fa-lg fa-paper-plane"></span><span class="fa fa-lg fa-comment"></span> <span class="fa fa-lg fa-user-circle"></span> <span class="fa fa-lg fa-street-view"></span> <span class="fa fa-lg fa-camera-retro"></span> <span class="fa fa-lg fa-info-circle"></span> <span class="fa fa-lg fa-times"></span></div></div>');
+    $.ajax({url: live2d_path+"waifu-tips.js", dataType:"script", cache: true, success: function() {
+        $.ajax({url: live2d_path+"live2d.min.js", dataType:"script", cache: true, success: function() {
+                $.ajax({url: live2d_path+"jquery-ui.min.js", dataType:"script", cache: true, success: function() {
 
-		if (type === "css") {
-			tag = document.createElement("link");
-			tag.rel = "stylesheet";
-			tag.href = url;
-		}
-		else if (type === "js") {
-			tag = document.createElement("script");
-			tag.src = url;
-		}
-		if (tag) {
-			tag.onload = () => resolve(url);
-			tag.onerror = () => reject(url);
-			document.head.appendChild(tag);
-		}
-	});
-}
+                    live2d_settings['hitokotoAPI'] = "hitokoto.cn";  // 一言 API 可选 'lwl12.com', 'hitokoto.cn', 'jinrishici.com'(古诗词)
+                    live2d_settings['modelId'] = 4;                  // 默认模型 ID
+                    live2d_settings['modelTexturesId'] = 0;          // 默认材质（服装） ID
+                    live2d_settings['modelStorage'] = false;         // 不储存模型 ID
+                    /* 在 initModel 前添加 */
+                    initModel(live2d_path+"waifu-tips.json");
+            }});
+        }});
+    }});
+} catch(err) { console.error("[错误] 请检查JQuery是否引入") }
 
-// 加载 waifu.css live2d.min.js waifu-tips.js
-if (screen.width >= 768) {
-	Promise.all([
-		loadExternalResource(live2d_path + "waifu.css", "css"),
-		loadExternalResource(live2d_path + "live2d.min.js", "js"),
-		loadExternalResource(live2d_path + "waifu-tips.js", "js")
-	]).then(() => {
-		initWidget({
-			waifuPath: live2d_path + "waifu-tips.json",
-			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
-		});
-	});
-}
-// initWidget 第一个参数为 waifu-tips.json 的路径，第二个参数为 API 地址
-// API 后端可自行搭建，参考 https://github.com/fghrsh/live2d_api
-// 初始化看板娘会自动加载指定目录下的 waifu-tips.json
 
-console.log(`
-  く__,.ヘヽ.        /  ,ー､ 〉
-           ＼ ', !-─‐-i  /  /´
-           ／｀ｰ'       L/／｀ヽ､
-         /   ／,   /|   ,   ,       ',
-       ｲ   / /-‐/  ｉ  L_ ﾊ ヽ!   i
-        ﾚ ﾍ 7ｲ｀ﾄ   ﾚ'ｧ-ﾄ､!ハ|   |
-          !,/7 '0'     ´0iソ|    |
-          |.从"    _     ,,,, / |./    |
-          ﾚ'| i＞.､,,__  _,.イ /   .i   |
-            ﾚ'| | / k_７_/ﾚ'ヽ,  ﾊ.  |
-              | |/i 〈|/   i  ,.ﾍ |  i  |
-             .|/ /  ｉ：    ﾍ!    ＼  |
-              kヽ>､ﾊ    _,.ﾍ､    /､!
-              !'〈//｀Ｔ´', ＼ ｀'7'ｰr'
-              ﾚ'ヽL__|___i,___,ンﾚ|ノ
-                  ﾄ-,/  |___./
-                  'ｰ'    !_,.:
-`);
+
+
