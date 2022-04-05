0854244fec58e3454dc96bc005dc2ef7a624fd57
geother <geother@foxmail.com>
2022-04-05 18:49:42 +0800

    Site updated: 2022-04-05 18:49:42

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -45,8 +45,8 @@ live2d_settings['modelRandMode']        = 'switch';     // 模型切换，可选
 live2d_settings['modelTexturesRandMode']= 'rand';       // 材质切换，可选 'rand'(随机), 'switch'(顺序)
 
 // 提示消息选项
-live2d_settings['showHitokoto']         = true;         // 显示一言
-live2d_settings['showF12Status']        = true;         // 显示加载状态
+live2d_settings['showHitokoto']         = false;         // 显示一言
+live2d_settings['showF12Status']        = false;         // 显示加载状态
 live2d_settings['showF12Message']       = false;        // 显示看板娘消息
 live2d_settings['showF12OpenMsg']       = true;         // 显示控制台打开提示
 live2d_settings['showCopyMessage']      = true;         // 显示 复制内容 提示
@@ -61,7 +61,7 @@ live2d_settings['waifuToolLine']        = '20px';       // 工具栏行高，例
 live2d_settings['waifuToolTop']         = '0px'         // 工具栏顶部边距，例如 '0px', '-60px'
 live2d_settings['waifuMinWidth']        = '768px';      // 面页小于 指定宽度 隐藏看板娘，例如 'disable'(禁用), '768px'
 live2d_settings['waifuEdgeSide']        = 'left:0';     // 看板娘贴边方向，例如 'left:0'(靠左 0px), 'right:30'(靠右 30px)
-live2d_settings['waifuDraggable']       = 'disable';    // 拖拽样式，例如 'disable'(禁用), 'axis-x'(只能水平拖拽), 'unlimited'(自由拖拽)
+live2d_settings['waifuDraggable']       = 'unlimited';    // 拖拽样式，例如 'disable'(禁用), 'axis-x'(只能水平拖拽), 'unlimited'(自由拖拽)
 live2d_settings['waifuDraggableRevert'] = false;         // 松开鼠标还原拖拽位置，可选 true(真), false(假)
 
 // 其他杂项设置
