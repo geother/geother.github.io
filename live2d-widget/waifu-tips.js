6647e2b38e50d7f6e4f8065dbda70027689e2edc
geother <geother@foxmail.com>
2022-09-23 07:19:49 +0000

    Site updated: 2022-09-23 07:19:49

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -22,7 +22,7 @@ window.live2d_settings = Array(); /*
 // 后端接口
 // //cdn.jsdelivr.net/gh/fghrsh/live2d_api/
 live2d_settings['modelAPI']             = '//live2d.fghrsh.net/api/';   // 自建 API 修改这里
-// live2d_settings['modelAPI']             = '//api.domain.com/live2d_api/';   // 自建 API 修改这里
+// live2d_settings['modelAPI']             = '//github.com/xiaoyou66/live2d-api/';   // 自建 API 修改这里
 live2d_settings['tipsMessage']          = 'waifu-tips.json';            // 同目录下可省略路径
 live2d_settings['hitokotoAPI']          = 'lwl12.com';                  // 一言 API，可选 'lwl12.com', 'hitokoto.cn', 'jinrishici.com'(古诗词)
 
